86e0ed3f62d2bbaf40f0e78a26ef453879dfa6ec
Danni_Git <dannizj03@gmail.com>
2025-12-01 17:06:32 -0500

    0.0.12
    - arregle el ui life
    - agrege sistema de mapa (falta img reales)
    - prueba de replicacion del shader del agua

---
+++ Assets/Game/Scripts/UI/Map/AreaType.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum AreaType
+{
+    CentroDeMando,
+    SalaCentral,
+    Hangar,
+    Dormitorios,
+    Baños
+}
