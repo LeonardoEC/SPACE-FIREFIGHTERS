a62f1f03a616238d22f3c1b3370edaa41b2fe156
LeonardoEC <LeonardoDanielAV@proton.me>
2025-12-03 15:43:47 -0300

    Sistema de puntos y salud del NPC conectados

--- Assets/demo/Script/Fire/Fire_Balls_Controller.cs
+++ Assets/demo/Script/Fire/Fire_Balls_Controller.cs
@@ -8,9 +8,21 @@ public class Fire_Balls_Controller : MonoBehaviour
     {
         minifireMovement();
     }
-    // PoolObject para controlar el spawn de bolas de fuego
+
+    private void OnDisable()
+    {
+
+        if (UIPoints.Instance != null)
+        {
+            UIPoints.Instance.AddPoints(100);
+        }
+
+        Debug.Log("Ganasta 100pts por destruir bola de fuego");
+    }
+
     void minifireMovement()
     {
         transform.Translate(new Vector3(0, 0, 1f) * 4f * Time.deltaTime);
     }
+
 }
