60c20bea8574213e250f4399009875ffbd2ae9d6
Danni_Git <dannizj03@gmail.com>
2025-12-09 17:29:54 -0500

    0.0.33
    se arreglo sonido de caminar y nombres

--- Assets/Game/Scripts/Sound/CharacterWalking.cs
+++ Assets/Game/Scripts/Sound/CharacterWalking.cs
@@ -23,22 +23,35 @@ public class CharacterWalking : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (_audSource.activeSelf && _playerMov.inAir)
-        {
-            _audSource.SetActive(false);
-        }
-       
+        //if (_audSource.activeSelf && _playerMov.inAir)
+        //{
+        //    _audSource.SetActive(false);
+        //}
 
-        if (!_audSource.activeSelf && isMoving) {
-            if (_playerMov.isPlayerGrounded)
+
+        //if (!_audSource.activeSelf && isMoving) {
+        //    if (_playerMov.isPlayerGrounded)
+        //    {
+        //        _audSource.SetActive(true);
+        //    }            
+        //}
+        //else if (_audSource.activeSelf && !isMoving){
+
+        //    _audSource.SetActive(false);
+        //}
+
+        if (!_photonView.IsMine)
+        {
+            if (!_audSource.activeSelf && isMoving)
             {
                 _audSource.SetActive(true);
-            }            
+            }
+            else if (_audSource.activeSelf && !isMoving)
+            {
+                _audSource.SetActive(false);
+            }
         }
-        else if (_audSource.activeSelf && !isMoving){
-
-            _audSource.SetActive(false);
-        }
+       
 
 
         if (_photonView.IsMine)
@@ -49,6 +62,25 @@ public class CharacterWalking : MonoBehaviour
             {
                 isMoving = currentlyMoving;
 
+            }
+
+            if (_audSource.activeSelf && _playerMov.inAir)
+            {
+                _audSource.SetActive(false);
+            }
+
+
+            if (!_audSource.activeSelf && isMoving)
+            {
+                if (_playerMov.isPlayerGrounded)
+                {
+                    _audSource.SetActive(true);
+                }
+            }
+            else if (_audSource.activeSelf && !isMoving)
+            {
+
+                _audSource.SetActive(false);
             }
 
             _photonView.RPC("RPC_SetIsWalking", RpcTarget.Others, isMoving);
