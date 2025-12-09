037415924178f584a67baa18aa951472b057c11c
Danni_Git <dannizj03@gmail.com>
2025-12-09 11:28:14 -0500

    0.0.31
    add sound

--- Assets/Game/Scripts/Character/DeleteThingsToPlayer.cs
+++ Assets/Game/Scripts/Character/DeleteThingsToPlayer.cs
@@ -11,13 +11,24 @@ public class DeleteThingsToPlayer : MonoBehaviour
     public GameObject _ownerObj;
     public GameObject _otherObj;
 
+    [Space(10)]
+    public Rigidbody rg;
+    public CapsuleCollider cc;
+    public Player_Movement pm;
+    public Player_Controller pc;
 
     // Start is called before the first frame update
     void Start()
     {
         if (!_pho.IsMine)
         {
+            Destroy(rg);
+            Destroy(cc);
+            Destroy(pm);
+            Destroy(pc);
+
             Destroy(_ownerObj);
+
         }
         else
         {
---
+++ Assets/Game/Scripts/Sound/CharacterWalking.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class CharacterWalking : MonoBehaviour
+{
+    [SerializeField] private GameObject _audSource;
+    [SerializeField] private PhotonView _photonView;
+    [SerializeField] private Player_Movement _playerMov;
+
+    [SerializeField] private bool isMoving;
+
+    public Rigidbody rb;
+
+    public float threshold = 0.01f;
+
+    void Start()
+    {
+        _audSource.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_audSource.activeSelf && _playerMov.inAir)
+        {
+            _audSource.SetActive(false);
+        }
+       
+
+        if (!_audSource.activeSelf && isMoving) {
+            if (_playerMov.isPlayerGrounded)
+            {
+                _audSource.SetActive(true);
+            }            
+        }
+        else if (_audSource.activeSelf && !isMoving){
+
+            _audSource.SetActive(false);
+        }
+
+
+        if (_photonView.IsMine)
+        {
+            bool currentlyMoving = rb.velocity.sqrMagnitude > threshold;
+
+            if (currentlyMoving != isMoving)
+            {
+                isMoving = currentlyMoving;
+
+            }
+
+            _photonView.RPC("RPC_SetIsWalking", RpcTarget.Others, isMoving);
+        }
+
+       
+    }
+
+
+    [PunRPC]
+    void RPC_SetIsWalking(bool value)
+    {
+        isMoving = value;
+    }
+}
